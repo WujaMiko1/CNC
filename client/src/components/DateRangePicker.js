dc9df27ad7eb7bbff0961c65e2c67379f0b66b2c
WujaMiko1 <mikolajlibowski@gmail.com>
2025-07-31 23:16:51 +0200

    Update DateRangePicker.js

--- client/src/components/DateRangePicker.js
+++ client/src/components/DateRangePicker.js
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Search } from "lucide-react";
 
-interface DateRangePickerProps {
   dateRange: {
     from;
     to;
