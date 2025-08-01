586c7713c05a01d53e1af9e19e3f7aff11bbd880
WujaMiko1 <mikolajlibowski@gmail.com>
2025-08-01 09:37:12 +0200

    Delete client/src/DateRangePicker.js

--- client/src/DateRangePicker.js
+++
@@ -1,3 +0,0 @@
-export default function DateRangePicker({ dateRange, onDateRangeChange }) {
-  return <div>Picker (from: {dateRange.from.toDateString()}, to: {dateRange.to.toDateString()})</div>;
-}
