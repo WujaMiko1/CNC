5da05331ea2c3e808415b63da1521ca6e632bb56
WujaMiko1 <mikolajlibowski@gmail.com>
2025-08-01 09:07:33 +0200

    Delete client/src/pages directory

--- client/src/pages/ProductionAnalysis.js
+++
@@ -1,8 +0,0 @@
-export default function ProductionAnalysis() {
-  return (
-    <div className="max-w-4xl mx-auto mt-10 p-6 bg-white shadow-lg rounded-xl">
-      <h1 className="text-2xl font-bold mb-4">Analiza Produkcji</h1>
-      <p className="text-gray-700">W tym miejscu będą wykresy i statystyki pokazujące czasy pracy maszyn, wydajność itp.</p>
-    </div>
-  );
-}
