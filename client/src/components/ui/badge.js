4bc93d47e0a586d2bae16ccb0dd3d9ff3319ad1b
WujaMiko1 <mikolajlibowski@gmail.com>
2025-08-01 08:56:01 +0200

    Delete client/src/components/ui directory

--- client/src/components/ui/badge.js
+++
@@ -1,7 +0,0 @@
-export default function Badge({ children, className = "" }) {
-  return (
-    <span className={`inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ${className}`}>
-      {children}
-    </span>
-  );
-}
