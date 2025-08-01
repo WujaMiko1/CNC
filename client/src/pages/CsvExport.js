d35653e0821e18a2e7552d5d27e53652def71304
WujaMiko1 <mikolajlibowski@gmail.com>
2025-08-01 10:13:40 +0200

    Update CsvExport.js

--- client/src/pages/CsvExport.js
+++ client/src/pages/CsvExport.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { DateRangePicker } from "@/components/DateRangePicker";
-import { ExportSection } from "@/components/SxportSection";
+import DateRangePicker from "@/components/DateRangePicker";
+import ExportSection from "@/components/SxportSection";
 import { Download, FileSpreadsheet, Settings } from "lucide-react";
 
 export default function CsvExport() {
